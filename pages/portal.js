67beac3f4e6391621f422e60cbebd250fc441f9d
kevmack7323-tech <kevmack7323@gmail.com>
2026-05-28 12:49:20 -0400

    Cache at least one element using selectElementById.

---
+++ pages/portal.js
@@ -0,0 +1,13 @@
+
+const loginFormContainer = document.getElementById("pContainer");
+loginFormContainer.style.color = "white";
+loginFormContainer.style.backgroundColor= "black"; 
+
+
+
+
+
+
+
+
+

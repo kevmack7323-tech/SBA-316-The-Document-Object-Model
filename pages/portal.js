83586e1510dde05f15f3df130d2cdbdc0c6ee21e
kevmack7323-tech <kevmack7323@gmail.com>
2026-05-28 13:31:57 -0400

    forgot password event listener

--- pages/portal.js
+++ pages/portal.js
@@ -3,8 +3,13 @@ const loginFormContainer = document.getElementById("pContainer");
 loginFormContainer.style.color = "white";
 loginFormContainer.style.backgroundColor= "black"; 
 
-
-
+document.querySelector("#Forgot").addEventListener("click", function(e){
+    e.preventDefault();
+    e.stopPropagation();
+        window.alert("Check Your Spaceship's Digital Serial Number For Galaxy Code")
+        const password = document.getElementById("password")
+        password.focus();
+})
 
 
 

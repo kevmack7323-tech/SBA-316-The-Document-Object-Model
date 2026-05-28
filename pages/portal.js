3450e05de2f293af045182f254d7cc4cc187669a
kevmack7323-tech <kevmack7323@gmail.com>
2026-05-28 14:27:43 -0400

    attribute h2 response to user action & firstChild

--- pages/portal.js
+++ pages/portal.js
@@ -2,6 +2,9 @@
 const loginFormContainer = document.getElementById("pContainer");
 loginFormContainer.style.color = "white";
 loginFormContainer.style.backgroundColor= "black"; 
+loginFormContainer.style.display = "flex";
+loginFormContainer.style.justifyContent = "center";
+loginFormContainer.style.alignItems= "center";
 
 document.querySelector("#Forgot").addEventListener("click", function(e){
     e.preventDefault();
@@ -11,6 +14,18 @@ document.querySelector("#Forgot").addEventListener("click", function(e){
         password.focus();
 })
 
+const zContainer = document.getElementById("zContainer");
+const firstKid = zContainer.firstElementChild;
+
+zContainer.style.display = "flex";
+zContainer.style.flexDirection = "column";
+zContainer.style.width = "100%";
+zContainer.style.alignItems= "center";
+zContainer.style.margin = "20px";
+
+firstKid.addEventListener("mouseover", function(e){
+   firstKid.style.color = "pink"; 
+})
 
 
 

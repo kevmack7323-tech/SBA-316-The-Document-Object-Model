d778362d6c74650553f9e46c1399daf14d236c82
kevmack7323-tech <kevmack7323@gmail.com>
2026-05-28 15:24:54 -0400

    added portal gif (creativity)

--- pages/portal.js
+++ pages/portal.js
@@ -1,17 +1,21 @@
 
 const loginFormContainer = document.getElementById("pContainer");
-loginFormContainer.style.color = "white";
-loginFormContainer.style.backgroundColor= "black"; 
+loginFormContainer.style.color = "yellow";
+loginFormContainer.style.backgroundColor = "white";
 loginFormContainer.style.display = "flex";
 loginFormContainer.style.justifyContent = "center";
-loginFormContainer.style.alignItems= "center";
+loginFormContainer.style.alignItems = "center";
+loginFormContainer.style.height="100vh"
+loginFormContainer.style.width="100vw"
+loginFormContainer.style.backgroundImage= "url('../images/portal.gif')"
 
-document.querySelector("#Forgot").addEventListener("click", function(e){
+
+document.querySelector("#Forgot").addEventListener("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
-        window.alert("Check Your Spaceship's Digital Serial Number For Galaxy Code")
-        const password = document.getElementById("password")
-        password.focus();
+    window.alert("Check Your Spaceship's Digital Serial Number For Galaxy Code")
+    const password = document.getElementById("password")
+    password.focus();
 })
 
 const zContainer = document.getElementById("zContainer");
@@ -19,12 +23,15 @@ const firstKid = zContainer.firstElementChild;
 
 zContainer.style.display = "flex";
 zContainer.style.flexDirection = "column";
-zContainer.style.width = "100%";
-zContainer.style.alignItems= "center";
+zContainer.style.width = "50%";
+zContainer.style.height= "50%"
+zContainer.style.alignItems = "center";
 zContainer.style.margin = "20px";
+zContainer.style.backgroundColor = ""
+zContainer.style.border= ""
 
-firstKid.addEventListener("mouseover", function(e){
-   firstKid.style.color = "pink"; 
+firstKid.addEventListener("mouseover", function (e) {
+    firstKid.style.color = "pink";
 })
 
 

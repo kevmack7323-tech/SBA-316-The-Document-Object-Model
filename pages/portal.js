1c1e033782f4981b792a5311a728f0f49d730660
kevmack7323-tech <kevmack7323@gmail.com>
2026-05-31 02:04:38 -0400

    Added Attribute Modifications

--- pages/portal.js
+++ pages/portal.js
@@ -13,6 +13,22 @@ loginFormContainer.style.backgroundSize= "cover";
 loginFormContainer.style.backgroundPosition= "center"; 
 loginFormContainer.style.backgroundAttachment= "fixed";
 
+const zContainer = document.getElementById("zContainer");
+const firstKid = zContainer.firstElementChild;
+
+zContainer.style.display = "flex";
+zContainer.style.flexDirection = "column";
+zContainer.style.width = "100%";
+zContainer.style.height = "auto"
+zContainer.style.alignItems = "center";
+zContainer.style.margin = "20px";
+zContainer.style.backgroundColor = "rgba(0, 0, 0, 0.75)"
+zContainer.style.border = "2px solid #93ff65"
+zContainer.style.borderRadius= "12px"
+zContainer.style.boxShadow = "0 0 20px rgba(0, 255, 204, 0.5)"
+zContainer.style.width = "380px";
+zContainer.style.padding = "30px"
+
 
 
 document.querySelector("#Forgot").addEventListener("click", function (e) {
@@ -21,25 +37,24 @@ document.querySelector("#Forgot").addEventListener("click", function (e) {
     window.alert("Check Your Spaceship's Digital Serial Number For Galaxy Code")
     const password = document.getElementById("password")
     password.focus();
-})
+});
 
-const zContainer = document.getElementById("zContainer");
-const firstKid = zContainer.firstElementChild;
-
-zContainer.style.display = "flex";
-zContainer.style.flexDirection = "column";
-zContainer.style.width = "50%";
-zContainer.style.height = "50%"
-zContainer.style.alignItems = "center";
-zContainer.style.margin = "20px";
-zContainer.style.backgroundColor = ""
-zContainer.style.border = ""
-zContainer.style.borderRadius= "30%"
 
 
 firstKid.addEventListener("mouseover", function (e) {
-    firstKid.style.color = "pink";
+    firstKid.style.color = "lightGreen";
+});
+
+firstKid.addEventListener("click", function(e){
+    firstKid.textContent= "Choose Galaxy Wisely"
+
+    const passwordInput= document.getElementById("password");
+    passwordInput.setAttribute("placeholder", "Enter 10+ character code")
+
 })
+
+
+
 
 const portalForm = document.querySelector(".container")
 

9ff139aaf890b5bb6623a5fbc7334601b5985891
kevmack7323-tech <kevmack7323@gmail.com>
2026-05-31 16:09:54 -0400

    document fragment created

--- pages/portal.js
+++ pages/portal.js
@@ -7,11 +7,11 @@ loginFormContainer.style.alignItems = "center";
 loginFormContainer.style.height = "100vh"
 loginFormContainer.style.width = "100vw"
 loginFormContainer.style.backgroundImage = "url('../images/greenport.gif')"
-loginFormContainer.style.backgroundPosition= "center"
-loginFormContainer.style.backgroundRepeat= "no-repeat"; 
-loginFormContainer.style.backgroundSize= "cover";   
-loginFormContainer.style.backgroundPosition= "center"; 
-loginFormContainer.style.backgroundAttachment= "fixed";
+loginFormContainer.style.backgroundPosition = "center"
+loginFormContainer.style.backgroundRepeat = "no-repeat";
+loginFormContainer.style.backgroundSize = "cover";
+loginFormContainer.style.backgroundPosition = "center";
+loginFormContainer.style.backgroundAttachment = "fixed";
 
 const zContainer = document.getElementById("zContainer");
 const firstKid = zContainer.firstElementChild;
@@ -24,7 +24,7 @@ zContainer.style.alignItems = "center";
 zContainer.style.margin = "20px";
 zContainer.style.backgroundColor = "rgba(0, 0, 0, 0.75)"
 zContainer.style.border = "2px solid #93ff65"
-zContainer.style.borderRadius= "12px"
+zContainer.style.borderRadius = "12px"
 zContainer.style.boxShadow = "0 0 20px rgba(0, 255, 204, 0.5)"
 zContainer.style.width = "380px";
 zContainer.style.padding = "30px"
@@ -45,13 +45,12 @@ firstKid.addEventListener("mouseover", function (e) {
     firstKid.style.color = "lightGreen";
 });
 
-firstKid.addEventListener("click", function(e){
-    firstKid.textContent= "Choose Galaxy Wisely"
+firstKid.addEventListener("click", function (e) {
+    firstKid.textContent = "Choose Galaxy Wisely"
 
-    const passwordInput= document.getElementById("password");
+    const passwordInput = document.getElementById("password");
     passwordInput.setAttribute("placeholder", "Enter 10+ character code")
-
-})
+});
 
 
 
@@ -59,37 +58,49 @@ firstKid.addEventListener("click", function(e){
 const portalForm = document.querySelector(".container")
 
 portalForm.addEventListener("submit", function (e) {
-        e.preventDefault();
-        e.stopPropagation();
+    e.preventDefault();
+    e.stopPropagation();
 
-        const inputValues = portalForm.querySelectorAll("input")
-        let isFormValid = true;
+    const inputValues = portalForm.querySelectorAll("input")
+    let isFormValid = true;
 
-        for (let i = 0; i < inputValues.length; i++) {
-            const currentInput = inputValues[i];
+    for (let i = 0; i < inputValues.length; i++) {
+        const currentInput = inputValues[i];
 
-            if (!currentInput.checkValidity()) {
-                isFormValid = false;
-                currentInput.reportValidity();
+        if (!currentInput.checkValidity()) {
+            isFormValid = false;
+            currentInput.reportValidity();
 
-                break;
-            }
+            break;
         }
-        if (isFormValid) {
-            window.open("https://github.com/kevmack7323-tech/SBA-316-The-Document-Object-Model/tree/main")
-        }
-    });
+    }
+    if (isFormValid) {
+        window.open("https://github.com/kevmack7323-tech/SBA-316-The-Document-Object-Model/tree/main")
+    }
+});
 
 
 
 
 const portalTwo = document.createElement("div");
 document.getElementById("pContainer").appendChild(portalTwo);
-portalTwo.style.backgroundImage= "url('../images/goodbye.gif')"
-portalTwo.style.height= "250px"
-portalTwo.style.width= "150px"
-portalTwo.style.borderRadius= "50%"
-portalTwo.style.backgroundPosition= "center"
+portalTwo.style.backgroundImage = "url('../images/goodbye.gif')"
+portalTwo.style.height = "250px"
+portalTwo.style.width = "150px"
+portalTwo.style.borderRadius = "50%"
+portalTwo.style.backgroundPosition = "center"
+
+const fragment = document.createDocumentFragment();
+
+
+
+
+
+
+
+
+
+
 
 
 
